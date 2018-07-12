91e266fbc188a207192c8ac5c5a1b19b28fe1a1e
Juergen Kreitler <juergen.kreitler@commerce-experts.com>
2018-07-12 09:50:30 +0200

    Add generic event collector handling custom events other than search result

---
+++ src/main/collectors/GenericEventCollector.js
@@ -0,0 +1,34 @@
+/**
+ * Collect different type of events via a custom event. The custom event should hold the properties
+ * "type" and "data" in the custom payload.
+ *
+ * See https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events for guidance
+ */
+class GenericEventCollector {
+
+  /**
+   * Construct event based collector
+   *
+   * @constructor
+   * @param {string} eventName - the name of the event to react on
+   */
+  constructor(eventName) {
+    this.eventName = eventName;
+  }
+
+  /**
+   * Attach a writer, note that this collector is asynchronous and will write
+   * the data when the event triggers
+   *
+   * @param {object} writer - The writer to send the data to
+   */
+  attach(writer) {
+    window.addEventListener(this.eventName, e => {
+      writer.write({
+        "type" : e.detail.type,
+        "data" : e.detail.data
+      });
+    })
+  }
+}
+module.exports = GenericEventCollector;
