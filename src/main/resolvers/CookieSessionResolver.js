8808cd9fe59b66bee4e035a2b11aab48854191de
Pavel Penchev <pavel.penchev@gmail.com>
2018-09-28 12:02:13 +0300

    Minor adjustement, no functional changes

--- src/main/resolvers/CookieSessionResolver.js
+++ src/main/resolvers/CookieSessionResolver.js
@@ -82,9 +82,10 @@ function generateId() {
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   var text = "";
-  for (var i = 0; i < 7; i++)
+  for (var i = 0; i < 7; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
-
+  }
+  
   return text;
 }
 
