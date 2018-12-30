3d08788c8c0e882cd8fcaef6f8f912bacc2a71b1
Pavel Penchev <pavel.penchev@gmail.com>
2018-12-30 21:41:35 +0200

    Add the possibility to store the logs directly on disk, store in /tmp by default with a parameter in package.json

--- src/test/util/server.js
+++ src/test/util/server.js
@@ -1,4 +1,6 @@
 var server = require("./http-server");
 
 var dataDir = process.argv[2]; // 0 and 1 are command and filename
-server.start(null, dataDir);
+var storeDir = process.argv[3];
+
+server.start(null, dataDir, storeDir);
