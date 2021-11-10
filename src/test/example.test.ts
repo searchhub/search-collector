97c2d79a4275b0bd47828ddedbb2779e876179dc
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-10 12:35:40 +0100

    SH-645 start and stop mock server automatically

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,10 +1,20 @@
 import {Page} from "puppeteer";
+import {shutdownMockServer, startMockServer} from "./util";
 
 declare const page: Page;
 
 const timeout = process.env.SLOWMO ? 30000 : 10000;
 
 describe('Test header and title of the page', () => {
+
+	beforeAll(async () => {
+		await startMockServer();
+	})
+
+	afterAll(() => {
+		shutdownMockServer();
+	})
+
 	test('Title of the page', async () => {
 		await page.goto("http://localhost:8081/example.page.html", {waitUntil: 'domcontentloaded'});
 		const title = await page.title();
