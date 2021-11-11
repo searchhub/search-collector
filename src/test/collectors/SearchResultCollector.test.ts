620ed9ccdbb6204f01d3b0c3257bce016f63325f
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-11 15:49:19 +0100

    SH-645 add wiremock assertion

--- src/test/collectors/SearchResultCollector.test.ts
+++ src/test/collectors/SearchResultCollector.test.ts
@@ -1,5 +1,5 @@
 import {Page} from "puppeteer";
-import {shutdownMockServer, startMockServer, verifyApiCallCount} from "../wiremock";
+import {createStubAsserter, shutdownMockServer, startMockServer} from "../wiremock";
 
 declare const page: Page;
 
@@ -13,12 +13,27 @@ describe('SearchResultCollector Suite', () => {
 		await shutdownMockServer();
 	})
 
-	beforeEach(async  () => {
+	beforeEach(async () => {
 
 	})
 
 	test('track search result data', async () => {
+		const stubAsserter = await createStubAsserter("SearchResultCollectorTracking.json");
+
 		await page.goto("http://localhost:8081/SearchResultCollector.page.html", {waitUntil: 'load'});
-		await verifyApiCallCount(1, "SearchResultCollectorTracking.json");
+
+		await stubAsserter.verifyCallCount(1)
+		await stubAsserter.verifyBody(body => expect(body).toBeDefined());
+		await stubAsserter.verifyHeaders(headers => expect(headers["Accept"]).toContain("image"));
+		await stubAsserter.verifyQueryParams(params => {
+			const trackingData = JSON.parse(params.data.values[0]);
+			expect(trackingData.type).toBe("search");
+			expect(trackingData.keywords).toBe("THE QUERY");
+			expect(trackingData.count).toBe(10);
+			expect(trackingData.timestamp).toBeDefined();
+			expect(trackingData.session).toBe("search-result-collector-session");
+			expect(trackingData.query).toBe("THE QUERY"); // TODO probably wrong
+			expect(params.data.values.length).toBe(1);
+		})
 	});
 });
--- src/test/wiremock.ts
+++ src/test/wiremock.ts
@@ -6,17 +6,19 @@ import {join} from "path";
 let process: ChildProcess;
 export const shutdownMockServer = async () => {
 	if (process) {
-		try {
-			await fetch(`http://localhost:8081/__admin/shutdown`, {
-				method: "POST"
-			});
-		} catch (e) {
-			console.error("Could not stop wiremock server: ", e);
-		}
-
 		process.kill("SIGINT");
 		process = void 0;
 	}
+
+	try {
+		const res = await fetch(`http://localhost:8081/__admin/shutdown`, {
+			method: "POST"
+		});
+	} catch (e) {
+		// nop
+	}
+
+	await waitForShutdown();
 }
 
 export const startMockServer = async () => {
@@ -28,10 +30,14 @@ export const startMockServer = async () => {
 	console.debug(`wiremock ready after ${readyTime}ms`);
 }
 
+export const waitForShutdown = async (delay: number = 100, retries: number = 50) => {
+	// TODO implement me
+	return wait(100);
+}
+
 export const waitForReadiness = async (delay: number = 100, retries: number = 50) => {
 	const origRetries = retries;
 	while (retries > 0) {
-
 		const isReady = await isWiremockReady();
 		if (isReady)
 			return delay * Math.min((origRetries - retries), 1);
@@ -49,6 +55,8 @@ export const isWiremockReady = async () => {
 			method: "GET"
 		});
 
+		await res.json();
+
 		if (res.status === 200)
 			return true;
 	} catch (e) {
@@ -58,6 +66,101 @@ export const isWiremockReady = async () => {
 	return false;
 }
 
+class StubAsserter {
+
+	disposed: boolean;
+
+	constructor(private readonly stub) {
+	}
+
+	async verifyCallCount(callCount: number) {
+		this.checkDisposed();
+		await verifyStubMappingCallCount(this.stub.id, callCount);
+		return this;
+	}
+
+	async verifyBody(assertFn: (body: any) => void) {
+		this.checkDisposed();
+		const entry = await this.fetchJournalEntry();
+		await assertFn(entry.request.body);
+		return this;
+	}
+
+	async verifyHeaders(assertFn: (headers: { [key: string]: string }) => void) {
+		this.checkDisposed();
+		const entry = await this.fetchJournalEntry();
+		await assertFn(entry.request.headers);
+		return this;
+	}
+
+	async verifyCookies(assertFn: (cookies: { [key: string]: string }) => void) {
+		this.checkDisposed();
+		const entry = await this.fetchJournalEntry();
+		await assertFn(entry.request.cookies);
+		return this;
+	}
+
+	async verifyRequest(assertFn: (request: any) => void) {
+		this.checkDisposed();
+		const entry = await this.fetchJournalEntry();
+		await assertFn(entry.request);
+		return this;
+	}
+
+	async verifyQueryParams(assertFn: (queryParams: { [key: string]: { key: string, values: Array<string> } }) => void) {
+		this.checkDisposed();
+		const entry = await this.fetchJournalEntry();
+		await assertFn(entry.request.queryParams);
+		return this;
+	}
+
+	private async fetchJournalEntry() {
+		const entry = (await getJournal()).requests.find(entry => entry.stubMapping.id === this.stub.id);
+		if (!entry)
+			throw Error(`Could not find stub for id ${this.stub.id}`);
+
+		return entry;
+	}
+
+	private async dispose() {
+		this.disposed = true;
+		//TODO implement me
+	}
+
+	private checkDisposed() {
+		if (this.disposed === true)
+			throw Error("This asserter is already disposed");
+	}
+}
+
+export const createStubAsserter = async (filename: string): Promise<StubAsserter> => {
+	const stub = await createStubMapping(filename);
+	return new StubAsserter(stub);
+}
+
+export const createStubMapping = async (filename: string) => {
+	const mapping: any = require(join(__dirname, "mock", "api-stubs", filename));
+
+	if (!mapping)
+		throw Error(`mapping ${filename} not found`);
+
+	const res = await fetch(`http://localhost:8081/__admin/mappings`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(mapping)
+	});
+
+	return await res.json();
+}
+
+export const verifyStubMappingCallCount = async (id: string, callCount: number) => {
+	const journal = await getJournal();
+	const called = journal.requests.reduce((acc, request) => request.stubMapping.id === id ? ++acc : acc, 0);
+	expect(called).toBe(callCount);
+}
+
 export const verifyApiCallCount = async (callCount: number, filename: string) => {
 	const mapping: any = require(join(__dirname, "mock", "mappings", filename));
 
@@ -65,7 +168,7 @@ export const verifyApiCallCount = async (callCount: number, filename: string) =>
 		throw Error(`mapping ${filename} not found`);
 
 	if (!mapping.request)
-		throw Error("mapping ${filename} doesnt contain a request section");
+		throw Error(`mapping ${filename} doesnt contain a request section`);
 
 	const res = await fetch(`http://localhost:8081/__admin/requests/count`, {
 		method: "POST",
