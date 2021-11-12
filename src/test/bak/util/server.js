23ba86a8d4fa12ef9502163bdddde798428d4eab
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-12 14:34:48 +0100

    SH-645 delete legacy tests

--- src/test/bak/util/server.js
+++
@@ -1,6 +0,0 @@
-var server = require("./http-server");
-
-var dataDir = process.argv[2]; // 0 and 1 are command and filename
-var storeDir = process.argv[3];
-
-server.start(null, dataDir, storeDir);
