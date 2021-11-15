3c9252eee42c0c390c8f6d182b9007488eefbb91
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-15 15:05:54 +0100

    SH-645 change storage key

--- src/main/collectors/RedirectCollector.ts
+++ src/main/collectors/RedirectCollector.ts
@@ -7,6 +7,8 @@ import {BooleanResolver, CallbackResolver} from "../resolvers/Resolver";
  */
 export class RedirectCollector extends AbstractCollector {
 
+	private static STORAGE_KEY = "__lastSearch";
+
 	private readonly triggerResolver: CallbackResolver;
 	private readonly expectedPageResolver: BooleanResolver;
 
@@ -28,19 +30,20 @@ export class RedirectCollector extends AbstractCollector {
 	 * Check whether we should be recording a redirect event
 	 *
 	 * @param {object} writer - The writer to send the data to
+	 * @param log
 	 */
 	attach(writer, log) {
 		const win = this.context.getWindow();
 
 		this.resolve(this.triggerResolver, log, keyword => {
-			win.sessionStorage.setItem("lastSearch", keyword);
+			win.sessionStorage.setItem(RedirectCollector.STORAGE_KEY, keyword);
 		});
 
 		// Fetch the latest search if any
-		const lastSearch = win.sessionStorage.getItem("lastSearch");
+		const lastSearch = win.sessionStorage.getItem(RedirectCollector.STORAGE_KEY);
 		if (lastSearch) {
 			// Remove the search action, as we're either on a search result page or we've redirected
-			win.sessionStorage.removeItem("lastSearch");
+			win.sessionStorage.removeItem(RedirectCollector.STORAGE_KEY);
 
 			// If we have not landed on the expected search page, it must have been (looove) a redirect
 			if (!this.resolve(this.expectedPageResolver, log)) {
