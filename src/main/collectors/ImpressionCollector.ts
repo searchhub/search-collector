5b76319200da5087640be535e7ee518d32ff7587
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-15 10:02:55 +0100

    SH-645 simplify ImpressionCollector + remove queue and timre

--- src/main/collectors/ImpressionCollector.ts
+++ src/main/collectors/ImpressionCollector.ts
@@ -1,8 +1,6 @@
 import {AbstractCollector} from "./AbstractCollector";
-import {LocalStorageQueue} from "../utils/LocalStorageQueue";
 import {Sentinel} from "../utils/Sentinel";
 import {scrollMonitor} from "scrollmonitor";
-import {Writer} from "../writers/Writer";
 import {NumberResolver, StringResolver} from "../resolvers/Resolver";
 
 //TODO change it maybe?
@@ -20,12 +18,8 @@ type ImpressionAttributeCollector = {
  * Handles both DOM elements present in the DOM and elements inserted after the page load / collector construction.
  */
 export class ImpressionCollector extends AbstractCollector {
-
 	selectorExpression: string;
 	attributeCollector: ImpressionAttributeCollector;
-	queue: LocalStorageQueue;
-	timer: NodeJS.Timeout;
-	writer: Writer;
 
 	/**
 	 * Construct impression collector
@@ -38,9 +32,6 @@ export class ImpressionCollector extends AbstractCollector {
 		super("impression");
 		this.selectorExpression = selectorExpression;
 		this.attributeCollector = attributeCollector;
-
-		this.queue = new LocalStorageQueue("impressions");
-		this.timer = setTimeout(this.flush.bind(this), 1000);
 	}
 
 	/**
@@ -50,43 +41,23 @@ export class ImpressionCollector extends AbstractCollector {
 	 * @param {object} writer - The writer to send the data to
 	 */
 	attach(writer) {
-		this.writer = writer;
-
-		const document = this.getDocument();
-		const window = this.getWindow();
-
 		const handler = el => {
-			const data = this.attributeCollector(el);
 			const watcher = scrollMonitor.create(el);
 
 			watcher.enterViewport(() => {
+				const data = {
+					type: this.type,
+					...this.attributeCollector(el)
+				};
 				// Figure out if we need to check the visibility of an element i.e.
 				// guard against elements that enter the viewport but have display=hidden
 				// if (el.offsetParent === null) {
 				//  return;
 				// }
-				this.queue.push(data);
+				writer.write(data);
 			})
 		};
 
 		new Sentinel(this.getDocument()).on(this.selectorExpression, handler);
-	}
-
-
-	flush(cancelTimer) {
-		if (this.queue.size() > 0) {
-			// TODO organize drain and write via callbacks to ensure no data is lost
-			// if the browser shutsdown before the write is complete
-			const data = this.queue.drain();
-
-			this.writer.write({
-				"type": "impression",
-				"data": data
-			});
-		}
-
-		if (!cancelTimer) {
-			this.timer = setTimeout(this.flush.bind(this), 1000);
-		}
 	}
 }
