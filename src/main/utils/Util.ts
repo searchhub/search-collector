fe0a74557fbde0326aeada5c8ecb87f225209ce9
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2021-11-25 15:05:21 +0100

    chg parse queryString

--- src/main/utils/Util.ts
+++ src/main/utils/Util.ts
@@ -2,23 +2,11 @@
  * Parse the browser query string or the passed string into a javascript object
  * with keys the query parameter names and values the corresponding values.
  *
- * @param {string} q - the query string to parse, window.location.search if not available
+ * @param {string} queryString - the query string to parse, window.location.search if not available
  * @return {object}
  */
-export const parseQueryString = (q = window.location.search.substring(1)) => {
-	const string = decodeURIComponent(q);
-
-	const queryString = {};
-	string.replace(
-		new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-		//TODO ts unknown overload?
-		//@ts-ignore
-		function ($0, $1, $2, $3) {
-			queryString[$1] = $3;
-		}
-	);
-
-	return queryString;
+export const parseQueryString = (queryString = window.location.search) => {
+	return new URLSearchParams(queryString);
 }
 
 /**
