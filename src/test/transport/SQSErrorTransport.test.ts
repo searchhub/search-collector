7260fb19b92ed6da52f1c4c20db23c241a77cdb9
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2022-01-11 12:57:32 +0100

    add error transport test

---
+++ src/test/transport/SQSErrorTransport.test.ts
@@ -0,0 +1,35 @@
+import {createStubAsserter, shutdownMockServer, startMockServer, verifyNoUnmatchedRequests, wait} from "../wiremock";
+import {Page} from "puppeteer";
+
+declare var page: Page;
+
+describe('Test the SQSErrorTransport', () => {
+
+	beforeAll(async () => {
+		await startMockServer();
+	})
+
+	afterAll(async () => {
+		await shutdownMockServer();
+	})
+
+	afterEach(async () => {
+		await verifyNoUnmatchedRequests();
+	})
+
+	test('DefaultWriter', async () => {
+		const asserter = await createStubAsserter("DefaultWriter.json");
+
+		await page.goto("http://localhost:8081/DefaultWriter.page.html", {waitUntil: 'load'});
+		await wait(1200); // wait more than a second for the buffering writer
+
+		await asserter.verifyCallCount(1)
+			.verifyQueryParams(params => {
+				expect(params["MessageBody"].values.length).toBe(1);
+				// timestamp is shimmed in browser to always be 555, this way we can just assert the encoded string
+				expect(params["MessageBody"].values[0]).toBe("JTVCJTdCJTIyZW50cnklMjIlM0ExJTJDJTIyZGF0YSUyMiUzQSUyMmRhdGElMjIlMkMlMjJ0aW1lc3RhbXAlMjIlM0E1NTUlMkMlMjJzZXNzaW9uJTIyJTNBJTIybXktc2Vzc2lvbiUyMiUyQyUyMmNoYW5uZWwlMjIlM0ElMjJkZWZhdWx0LXdyaXRlci1jaGFubmVsJTIyJTJDJTIycXVlcnklMjIlM0ElMjIlMjIlN0QlMkMlN0IlMjJlbnRyeSUyMiUzQTIlMkMlMjJkYXRhJTIyJTNBJTIyZGF0YSUyMiUyQyUyMnRpbWVzdGFtcCUyMiUzQTU1NSUyQyUyMnNlc3Npb24lMjIlM0ElMjJteS1zZXNzaW9uJTIyJTJDJTIyY2hhbm5lbCUyMiUzQSUyMmRlZmF1bHQtd3JpdGVyLWNoYW5uZWwlMjIlMkMlMjJxdWVyeSUyMiUzQSUyMiUyMiU3RCUyQyU3QiUyMmVudHJ5JTIyJTNBMyUyQyUyMmRhdGElMjIlM0ElMjJkYXRhJTIyJTJDJTIydGltZXN0YW1wJTIyJTNBNTU1JTJDJTIyc2Vzc2lvbiUyMiUzQSUyMm15LXNlc3Npb24lMjIlMkMlMjJjaGFubmVsJTIyJTNBJTIyZGVmYXVsdC13cml0ZXItY2hhbm5lbCUyMiUyQyUyMnF1ZXJ5JTIyJTNBJTIyJTIyJTdEJTVE");
+			})
+			.verify();
+	})
+
+});
