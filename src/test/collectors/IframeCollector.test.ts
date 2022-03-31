26093ce7e05730c338eeb000c5aa0138efc784d6
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2022-03-31 10:43:16 +0200

    add iframe test

---
+++ src/test/collectors/IframeCollector.test.ts
@@ -0,0 +1,44 @@
+import {Page} from "puppeteer";
+import {createMockServer} from "../wiremock";
+import {wait} from "../util";
+
+declare const page: Page;
+
+describe('IframeCollector Suite', () => {
+
+	const {
+		startMockServer,
+		shutdownMockServer,
+		verifyNoUnmatchedRequests,
+		createStubAsserter,
+		getHost
+	} = createMockServer();
+
+	beforeAll(async () => {
+		await startMockServer();
+	})
+
+	afterAll(async () => {
+		await shutdownMockServer();
+	})
+
+	afterEach(async () => {
+		await verifyNoUnmatchedRequests();
+	})
+
+	test('execute from within iframe', async () => {
+		const asserter = await createStubAsserter("IframeCollectorTracking.json");
+
+		await page.goto(getHost() + "/ContextTestMain.page.html", {waitUntil: 'load'});
+		await page.click("#clickMe");
+		await wait(100);
+
+		await asserter.verifyCallCount(1)
+			.verifyQueryParams(params => {
+				const trackingData = JSON.parse(params.data.values[0]);
+				expect(trackingData.type).toBe("product");
+				expect(trackingData.id).toBe("5");
+			})
+			.verify();
+	});
+});
