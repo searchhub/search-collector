73019b6dd344237151752822683d84125ed0f107
Tobias Armbruster <tobias.armbruster@commerce-experts.com>
2022-05-12 10:32:52 +0200

    add optional expected page resolver to ImpressionsCollector

--- src/test/collectors/ImpressionCollector.test.ts
+++ src/test/collectors/ImpressionCollector.test.ts
@@ -49,4 +49,40 @@ describe('ImpressionCollectorTracking Suite', () => {
 			})
 			.verify();
 	});
+
+	test('track impression data with expected page false', async () => {
+		const stubAsserter = await createStubAsserter("ImpressionCollectorTracking_expected_false.json");
+
+		await page.goto(getHost() + "/ImpressionCollector.page.html?expectedPage=false", {waitUntil: 'load'});
+		await page.click("#scrollTarget");//click scrolls the element into view
+
+		await wait(750); //ImpressionCollector is debounced, give it some more time
+
+		await stubAsserter.verifyCallCount(0)
+			.verify();
+	});
+
+	test('track impression data with expected page true', async () => {
+		const stubAsserter = await createStubAsserter("ImpressionCollectorTracking_expected_true.json");
+
+		await page.goto(getHost() + "/ImpressionCollector.page.html?expectedPage=true", {waitUntil: 'load'});
+		await page.click("#scrollTarget");//click scrolls the element into view
+
+		await wait(750); //ImpressionCollector is debounced, give it some more time
+
+		await stubAsserter.verifyCallCount(1)
+			.verifyQueryParams(params => {
+				const event = JSON.parse(params.data.values[0]);
+				expect(event.type).toBe("impression");
+				expect(event.data.length).toBe(10)
+
+				event.data.forEach(impression => {
+					expect(impression.position).toBeDefined();
+					expect(typeof impression.position).toBe("number");
+					expect(impression.id).toBeDefined();
+					expect(typeof impression.id).toBe("string");
+				});
+			})
+			.verify();
+	});
 });
