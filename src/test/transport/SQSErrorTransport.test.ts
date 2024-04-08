0efead03b62f850c7731f765d8e3f038745475d4
Tobias Armbruster <armbruster.tobias@gmx.de>
2024-04-08 11:14:26 +0200

    fix message body hash

--- src/test/transport/SQSErrorTransport.test.ts
+++ src/test/transport/SQSErrorTransport.test.ts
@@ -36,7 +36,7 @@ describe('Test the SQSErrorTransport', () => {
 			.verifyQueryParams(params => {
 				expect(params["MessageBody"].values.length).toBe(1);
 				// timestamp is shimmed in browser to always be 555, this way we can just assert the encoded string
-				expect(params["MessageBody"].values[0]).toBe("JTVCJTdCJTIydHlwZSUyMiUzQSUyMmVycm9yJTIyJTJDJTIybXNnJTIyJTNBJTIyc29tZSUyMGVycm9yJTIwbWVzc2FnZSUyMiUyQyUyMmNoYW5uZWwlMjIlM0ElMjJkZWZhdWx0LXdyaXRlci1jaGFubmVsJTIyJTJDJTIyc2Vzc2lvbiUyMiUzQSUyMm15LXNlc3Npb24lMjIlMkMlMjJ0aW1lc3RhbXAlMjIlM0E1NTUlN0QlNUQ=");
+				expect(params["MessageBody"].values[0]).toBe("JTVCJTdCJTIydHlwZSUyMiUzQSUyMmVycm9yJTIyJTJDJTIybXNnJTIyJTNBJTIyc29tZSUyMGVycm9yJTIwbWVzc2FnZSUyMiUyQyUyMmNoYW5uZWwlMjIlM0ElMjJkZWZhdWx0LXdyaXRlci1jaGFubmVsJTIyJTJDJTIyc2Vzc2lvbiUyMiUzQSUyMm15LXNlc3Npb24lMjIlMkMlMjJ0aW1lc3RhbXAlMjIlM0E1NTUlMkMlMjJhcmd1bWVudHMlMjIlM0ElNUIlNUQlMkMlMjJ1cmwlMjIlM0ElMjJodHRwJTNBJTJGJTJGbG9jYWxob3N0JTNBNTE3MTklMkZTUVNFcnJvclRyYW5zcG9ydC5wYWdlLmh0bWwlMjIlMkMlMjJyZWZlcnJlciUyMiUzQSUyMiUyMiUyQyUyMmxhbmclMjIlM0ElMjJlbi1VUyUyMiU3RCU1RA==");
 			})
 			.verify();
 	})
